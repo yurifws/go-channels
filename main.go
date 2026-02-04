007d96aba0e00e2c74f9154910ea903dfa4055b0
yurifws <yurifws@gmail.com>
2026-02-03 23:04:04 -0300

    <feat GPL-084> channel implementation

--- main.go
+++ main.go
@@ -15,17 +15,23 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string)
+
 	for _, link := range links {
-		go checkLink(link)
+		go checkLink(link, c)
 	}
+
+	fmt.Println(<- c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		return
 	}
 
 	fmt.Println(link, "is up!")
+		c <- "Might be up I think"
 }
