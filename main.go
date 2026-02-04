4d38d00a91994b252d46bb86c0e2c79738aeea89
yurifws <yurifws@gmail.com>
2026-02-03 22:55:56 -0300

    <feat GPL-081> go routines

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 	}
 
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link)
 	}
 }
 
@@ -26,6 +26,6 @@ func checkLink(link string) {
 		fmt.Println(link, "might be down!")
 		return
 	}
-	
+
 	fmt.Println(link, "is up!")
 }
