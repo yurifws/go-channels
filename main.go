fe72dc1006a2761480adeca0205512060ffc2823
yurifws <yurifws@gmail.com>
2026-02-03 23:15:37 -0300

    <feat GPL-086> receiving messages

--- main.go
+++ main.go
@@ -21,11 +21,9 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	fmt.Println(<- c)
-	fmt.Println(<- c)
-	fmt.Println(<- c)
-	fmt.Println(<- c)
-	fmt.Println(<- c)
+	for i := 0; i < len(links); i++ {
+		fmt.Println(<- c)
+	}
 }
 
 func checkLink(link string, c chan string) {
